dfce8af3da400d0f49965308032fda91baae9a9a
Gagan deep <ext-gagan.deep@here.com>
2024-11-06 21:28:34 +0530

    shift from xampp to laragon

--- backend/routes/storeDoctorDetails.js
+++ backend/routes/storeDoctorDetails.js
@@ -5,16 +5,17 @@ const verifyToken = require('../middleware/verifytoken')
 
 router.post('/', verifyToken,  (req, res) => {
     // console.log(req.body);
-    const {dname, dage, dphone, demail, dspecification, date} = req.body;
+    const date = new Date();
+    const {dname, dage, dphone, demail, dspecification} = req.body;
     if (!dname || !dage || !dphone || !demail || !dspecification) {
-      console.log("inside if");
+      // console.log("inside if");
       return res.status(500).json({error: "message", message:"The input field cannot be empty"});
     }
     else{
       connection.query('INSERT INTO doctordetails (name, age, phone, email, specification, date) VALUES (?, ?, ?, ?, ?, ?)', [dname, dage, dphone, demail, dspecification, date], async(error, result) => {
-        console.log("inside query");
+        // console.log("inside query");
         if (error) {
-          throw error;
+          throw error; 
         }
         else{
           return res.status(200).json({ status: "success", success: "Data udated successfully" });
